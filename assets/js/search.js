29ae89827408bf2bfa349cf8ca8bf1b587dd9df2
JudePark96 <JudePark96@users.noreply.github.com>
2020-06-19 15:18:12 +0000

    deploy: c13f2e9c57019e009b2354b5f3e5d2f021a57809

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "https://JudePark96.github.io/about/",
     "title": "About Me",
-    "body": "Eunhwan Park Seoul, Republic of Korea. judepark@kookmin. ac. kr RESEARCH INTERESTS: Language Understanding, Dialogue System, Recommendation System, Graph Representaiton, Knowledge Distillation, etc EDUCATION:  BS in Computer Science, 2015 ~ 2016 – Hanbat National University BS in Computer Science, 2016 ~ present – Kookmin UniversityWork Experience:  Backend Engineer, Plating Co, 2015 ~ 2016 Backend Engineer, Sellmate Co, 2018 ~ 2019 Backend Engineer, FreeLancer, 2019PUBLICATIONS: Domestic Conference  Comparison of Varient BERT Model on Korean, Roje Lee, Eunhwan Park, Jaekoo Lee. KICS Winter Conference 2020TEACHING EXPERIENCE:  Daily Mentor, Galmae Middle School     Introducing Software Engineering   HONORS / AWARDS:  Software Maestro 6th, 2015.  Summer Innovation Program - University of California, Irvine"
+    "body": "Eunhwan Park Seoul, Republic of Korea. judepark@kookmin. ac. kr Introduction: Hi, I am student in the Department of Computer Science and Engineering at Kookmin University. If you want to know about me in detail, please see my curriculum vitae "
     }, {
     "id": 2,
     "url": "https://JudePark96.github.io/categories/",
